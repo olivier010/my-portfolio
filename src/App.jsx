accd4a13cd14213337353e8909b269d590152584
olivier010 <oliviernsengiyumva010@gmail.com>
2026-02-17 21:12:17 +0200

    updated footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-git// src/App.jsx
+// src/App.jsx
 import React, { useEffect, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence, motion } from 'framer-motion';
